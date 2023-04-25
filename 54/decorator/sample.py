517d5d38c52782888271305c38a61df55c0b5833
iwmstjp <miwaura95@gmail.com>
2023-04-25 22:01:02 +0200

    create file 54

---
+++ 54/decorator/sample.py
@@ -0,0 +1,9 @@
+def add(n1, n2):
+    return n1 + n2
+def sub(n1, n2):
+    return n1 - n2
+def multi(n1, n2):
+    return n1 * n2
+def cal(cal_f, n1, n2):
+    return cal_f(n1, n2)
+print(cal(add, 3, 5))
