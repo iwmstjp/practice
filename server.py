3b4611839a88b75368134a0a04cf75a683f286e8
iwmstjp <miwaura95@gmail.com>
2023-10-26 21:48:19 +0200

    Added files

--- server.py
+++ server.py
@@ -1,11 +1,16 @@
 from flask import Flask
 from flask import render_template
+import requests
 
+url = "https://api.npoint.io/7f61d8933e17ba5e2424"
+response = requests.get(url)
+# print(response.json())
+posts = response.json()
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", posts=posts)
 
 @app.route("/about")
 def about():
@@ -14,6 +19,14 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+@app.route("/post/<int:index>")
+def post(index):
+    request_post = None
+    for post in posts:
+        if post["id"] == index:
+            request_post = post
+    return render_template("post.html", post=request_post)
 
 if __name__ == "__main__":
     app.run(debug=True)
