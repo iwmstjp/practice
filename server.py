9ddbc1c4c5acbd94ecf9c00017b00a46b52ddc55
iwmstjp <miwaura95@gmail.com>
2023-11-04 20:13:41 +0100

    Modify server.py

--- server.py
+++ server.py
@@ -3,9 +3,27 @@ from flask import render_template, request
 import requests
 
 url = "https://api.npoint.io/7f61d8933e17ba5e2424"
-response = requests.get(url)
+# response = requests.get(url)
 # print(response.json())
-posts = response.json()
+# posts = response.json()
+posts=[
+    {
+        "id":1,
+        "title":"The life of Cacuts",
+        "subtitle":"Who knows",
+        "body":"A cactus (pl.: cacti, cactuses, or less commonly, cactus)[3] is a member of the plant family Cactaceae (/kæˈkteɪsiaɪ, -siːiː/),\
+        [a] a family comprising about 127 genera with some 1,750 known species of the order Caryophyllales.[4] The word cactus derives, through Latin, \
+        from the Ancient Greek word κάκτος (káktos), a name originally used by Theophrastus for a spiny plant whose identity is now not certain "
+    },
+    {
+        "id":2,
+        "title":"The life of Cats",
+        "subtitle":"strage of life",
+        "body":"The cat (Felis catus), commonly referred to as the domestic cat or house cat, is the only domesticated species in the family Felidae.\
+        Recent advances in archaeology and genetics have shown that the domestication of the cat occurred in the Near East around 7500 BC. It is commonly\
+        kept as a house pet and farm cat, but also ranges freely as a feral cat avoiding human contact. It is valued by humans for companionship and its ability to kill vermin."
+    }
+]
 app = Flask(__name__)
 
 @app.route("/")
@@ -16,14 +34,15 @@ def home():
 def about():
     return render_template("about.html")
 
-@app.route("/contact")
+@app.route("/contact", methods=['get','post'])
 def contact():
-    return render_template("contact.html")
-
-@app.route("/form-entry", methods=['post'])
-def recieve_data():
-    print(request.form.get('name'))
-    return "Successfully sent your message"
+    flag = False
+    if request.method == 'POST':
+        print(request.form.get('name'))
+        flag = True
+        return render_template("contact.html", flag=flag)
+    else:
+        return render_template("contact.html", flag=flag)
 
 
 @app.route("/post/<int:index>")
