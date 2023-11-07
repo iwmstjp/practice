416a3cf82e3b31dab57eb175f109f66980a1618a
iwmstjp <miwaura95@gmail.com>
2023-11-07 16:41:17 +0100

    Edit  main.py

--- movies/main.py
+++ movies/main.py
@@ -31,7 +31,8 @@ with app.app_context():
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    movies = db.session.execute(db.select(Movie).order_by(Movie.ranking.desc())).scalars()
+    return render_template("index.html", movies=movies)
 
 
 if __name__ == '__main__':
