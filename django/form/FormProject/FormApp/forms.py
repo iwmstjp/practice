d13aacb44a121f86d095e40fef3dedbbee8f9f38
Iwaura Masato <miwaura95@gmail.com>
2024-04-13 22:26:38 +0200

    section8:173 review

--- django/form/FormProject/FormApp/forms.py
+++ django/form/FormProject/FormApp/forms.py
@@ -4,7 +4,9 @@ from django import forms
 class UserInfo(forms.Form):
     name = forms.CharField(label='Na', max_length=10, min_length=5)
     age = forms.IntegerField()
-    mail = forms.EmailField(widget=forms.TextInput(attrs={'placeholder':'@mail.com'}))
+    mail = forms.EmailField(widget=forms.TextInput(
+        attrs={'class':'mail-class', 'placeholder':'@mail.com'})
+    )
     is_married = forms.BooleanField(required=False)
     birthday = forms.DateField(initial='1990-01-01')
     salary = forms.IntegerField()
@@ -14,10 +16,16 @@ class UserInfo(forms.Form):
         (3, 'student'),
         (4, 'unemployed')
     ))
-    hobby = forms.MultipleChoiceField(choices=(
+    hobbies = forms.MultipleChoiceField(choices=(
         (1, 'sports'),
         (2, 'reading'),
         (3, 'movies'),
         (4, 'etc')
     ),widget=forms.CheckboxSelectMultiple)
     memo = forms.CharField(widget=forms.Textarea)
+
+    def __init__(self, *args, **kwargs):
+        super(UserInfo, self).__init__(*args, **kwargs)
+        self.fields['job'].widget.attrs['id'] = 'id_job'
+        self.fields['hobbies'].widget.attrs['class'] = 'hobbies_class'
+
