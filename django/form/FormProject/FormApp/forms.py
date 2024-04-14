a7eb8397bc1aa7490763f1f6bbc1fffbeefa45ee
Iwaura Masato <miwaura95@gmail.com>
2024-04-14 22:23:01 +0200

    section8:173

--- django/form/FormProject/FormApp/forms.py
+++ django/form/FormProject/FormApp/forms.py
@@ -1,11 +1,16 @@
 from django import forms
-
+from django.core import validators
 
 class UserInfo(forms.Form):
     name = forms.CharField(label='Na', max_length=10, min_length=5)
-    age = forms.IntegerField()
+    age = forms.IntegerField(validators=[validators.MinValueValidator(20, message='More than 20')])
     mail = forms.EmailField(widget=forms.TextInput(
-        attrs={'class':'mail-class', 'placeholder':'@mail.com'})
+        attrs={'class':'mail-class', 'placeholder': '@mail.com'})
+    )
+    verify_mail = forms.EmailField(
+        label='confirm email',
+        widget=forms.TextInput(
+        attrs={'class': 'mail-class', 'placeholder': '@mail.com'})
     )
     is_married = forms.BooleanField(required=False)
     birthday = forms.DateField(initial='1990-01-01')
@@ -22,6 +27,7 @@ class UserInfo(forms.Form):
         (3, 'movies'),
         (4, 'etc')
     ),widget=forms.CheckboxSelectMultiple)
+    homepage = forms.URLField(required=False)
     memo = forms.CharField(widget=forms.Textarea)
 
     def __init__(self, *args, **kwargs):
@@ -29,3 +35,14 @@ class UserInfo(forms.Form):
         self.fields['job'].widget.attrs['id'] = 'id_job'
         self.fields['hobbies'].widget.attrs['class'] = 'hobbies_class'
 
+    def clean_homepage(self):
+        homepage = self.cleaned_data['homepage']
+        if not homepage.startswith('https'):
+            raise forms.ValidationError('Only https')
+
+    def clean(self):
+        cleaned_data = super().clean()
+        mail = cleaned_data['mail']
+        verify_mail = cleaned_data['verify_mail']
+        if mail != verify_mail:
+            raise forms.ValidationError('Type same email address')
