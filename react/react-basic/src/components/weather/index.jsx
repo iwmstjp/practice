7ea5cd845b5093d974e598eb45652e5aaeca7228
iwmst <miwaura95@gmail.com>
2024-04-23 19:32:57 +0200

    React: weather app

--- react/react-basic/src/components/weather/index.jsx
+++ react/react-basic/src/components/weather/index.jsx
@@ -1,43 +1,69 @@
 import { useEffect, useState } from "react";
 import Search from "../search";
 
-export default function Weather(){
-    const [search, setSearch] = useState('');
-    const [loading, setLoading] = useState(false);
-    const [weatherData, setWeatherData] = useState(null);
+export default function Weather() {
+  const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [weatherData, setWeatherData] = useState(null);
 
-    async function fetchWeatherData(param){
-        setLoading(true)
-        try{
-            const response = await fetch(
-                `https://api.openweathermap.org/data/2.5/weather?q=${param}&appid=5e157b12a85a9c1de074d745226a415f`)
-            const data = await response.json();
-            if(data){
-                setWeatherData(data);
-                setLoading(false);
-            }
-        }catch(e){
-            setLoading(false);
-            console.log(e);
-        }
+  async function fetchWeatherData(param) {
+    setLoading(true);
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${param}&appid=5e157b12a85a9c1de074d745226a415f`
+      );
+      const data = await response.json();
+      if (data) {
+        setWeatherData(data);
+        setLoading(false);
+        console.log(data);
+      }
+    } catch (e) {
+      setLoading(false);
+      console.log(e);
     }
+  }
 
-    function handleSearch(){
-        console.log(search)
-        fetchWeatherData(search)
-    };
-    useEffect(
-        ()=>{
-            fetchWeatherData("tokyo")
-        },[]
-    )
+  function handleSearch() {
+    // console.log(search);
+    fetchWeatherData(search);
+  }
+  useEffect(() => {
+    fetchWeatherData("tokyo");
+  }, []);
 
-    return <div>
-        <Search
+  return (
+    <div>
+      <Search
         search={search}
         setSearch={setSearch}
         handleSearch={handleSearch}
-        />
-        Weather
+      />
+      {loading ? (
+        <div className="loading">Loading</div>
+      ) : (
+        <div>
+          <div className="city-name">
+            {weatherData?.name} <span>{weatherData?.sys?.country}</span>
+          </div>
+          <div className="temp">{weatherData?.main?.temp}</div>
+          <p className="description">
+            {weatherData && weatherData.weather && weatherData.weather[0]
+              ? weatherData.weather[0].description
+              : ""}
+          </p>
+          <div className="weather-info">
+            <div className="column">
+              <p className="wind">{weatherData?.wind?.speed}</p>
+              <p>Wind Speed</p>
+            </div>
+            <div className="column">
+              <p className="humidity">{weatherData?.main?.humidity}</p>
+              <p>Humidity</p>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
+  );
 }
